18b318582b1c006b4bf2cb814c23793a8b88f100
krokkie <krokkie@users.noreply.github.com>
2021-08-19 19:52:10 +0200

    First steps of scraping the web

--- scripts/daily_prov_scraper.R
+++ scripts/daily_prov_scraper.R
@@ -1,5 +1,28 @@
-# R script details to go here....
+# Author: gerh@rd.co.za
+# Script that scrapes the sacoronavirus.co.za website
+# https://sacoronavirus.co.za/covid-19-daily-cases/
 
-warning("Under construction")
+print(getwd())
+stop("debug")
 
-stop("failure - testing email notification")
+rss <- xml2::as_list(xml2::read_xml(httr::GET("https://sacoronavirus.co.za/feed/")))[[1]][[1]]
+junk <- sapply(rss, function(x) (is.null(x$category)) || (unlist(x$category)!="Daily Cases"))
+rss <- rss[!junk]
+origtitle <- sapply(rss, getElement, "title") %>%
+  gsub(".*\\((.*)\\)", "\\1", .) %>% 
+  trimws()
+
+pubDate <- sapply(rss, getElement, "pubDate")
+# fix mistakes here
+origtitle <- gsub("Tuesday 09 August 2021", "Tuesday 10 August 2021", origtitle)
+
+names(rss) <- origtitle %>%   # only keep that in the brackets
+  as.Date.character(format = "%A %d %B %Y") %>%
+  as.character()
+
+if (any(duplicated(names(rss)))) {
+  stop("Duplicates found in the title of the daily cases - please check these")
+}
+
+rss <- lapply(rss, function(x) unlist(x$encoded))
+
